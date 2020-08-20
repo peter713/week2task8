f882b666a4e0b9211ed8cfc03df5c2b6c3acd744
peter713 <plaszuk@onet.eu>
2020-08-20 21:21:55 +0200

    commit week2task8sub3 Fibonacci Series

---
+++ week2task8sub3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace week2task8sub3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = 0, secondNum = 1, thirdNum;
+            Console.WriteLine("Please give last index that you want to get from Fibonacci series");
+            string strNum = Console.ReadLine();
+            bool isNum = int.TryParse(strNum, out int lastNum);
+            Console.Write(firstNum +" "+secondNum);
+            if (isNum)
+            {
+                for (int i = 2; i < lastNum; i++)
+                {
+                    thirdNum = firstNum + secondNum;
+                    Console.Write( " " + thirdNum );
+                    firstNum= secondNum;
+                   secondNum = thirdNum ;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

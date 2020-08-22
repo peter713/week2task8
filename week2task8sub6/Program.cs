a4596ce1d5ae3b0b3240aa9d113663acb8259592
peter713 <plaszuk@onet.eu>
2020-08-22 21:53:15 +0200

    commit regading wee2task8sub6

---
+++ week2task8sub6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace week2task8sub6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//sum of 1+1/2+1/3+...+1/20
+            double sum = 0;
+
+            for (double nextNum = 1; nextNum <= 20; nextNum++)
+            {
+                sum += (1 / nextNum);
+
+            }
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}

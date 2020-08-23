1fc505cccab2f1b996b7b125c34764641077b742
peter713 <plaszuk@onet.eu>
2020-08-23 20:53:38 +0200

    commit regarding week2task8sub8_

---
+++ week2task8sub7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week2task8sub7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ week2task8sub8_/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace week2task8sub8_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//reverse string
+            Console.WriteLine("Provide string of chracters");
+            string str = Console.ReadLine();
+            //string word = "Abcdefg";
+            char[] array = str.ToCharArray();
+            Array.Reverse(array); 
+            Console.WriteLine(array);
+
+            Console.ReadKey();
+        }
+    }
+}

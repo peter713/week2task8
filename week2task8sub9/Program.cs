a75263f999e5f09f4c3b6bae41b6441dd69952bd
peter713 <plaszuk@onet.eu>
2020-08-23 21:37:38 +0200

    commit week2task8sub9 - not demical but int

---
+++ week2task8sub9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace week2task8sub9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This app will covert your number on binary, type in number:");
+            int num = int.Parse(Console.ReadLine());
+            int reminder;
+            string result = " ";
+             Console.Write($"\r\nBinary of {num} is equal to ");
+            while (num > 0)
+            {
+                reminder = num % 2;
+                num /= 2; 
+                result = reminder.ToString() + result; 
+            }
+
+            Console.Write(result); 
+
+            Console.ReadKey();
+        }
+    }
+}

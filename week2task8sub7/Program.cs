df9cb1e01575b60cd6c82e1ccb021498aef69889
peter713 <plaszuk@onet.eu>
2020-08-27 22:15:55 +0200

    wee2task8sub7 at last

--- week2task8sub7/Program.cs
+++ week2task8sub7/Program.cs
@@ -6,7 +6,35 @@ namespace week2task8sub7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Provide lenght of the diaginal as positive integer number, then app will draw the daimond:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 1; i <= n - 1; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = n - 1; k >= i; k--)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
